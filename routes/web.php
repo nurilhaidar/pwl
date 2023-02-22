f9e1c5af9572c8b6b89c3a6e4735a1b7d682588f
Mukhammad Nuril Haidar <nurilhaidar15@gmail.com>
2023-02-22 17:18:11 +0700

    Nuril 02_praktikum_web_lanjut_satu

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    echo "Selamat Datang ! ";
+});
+
+Route::get('/about', function () {
+    echo "NIM  : 2141720208";
+    echo "Nama : Mukhammad Nuril Haidar";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman artikel dengan id $id";
+});
